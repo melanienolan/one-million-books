e85e7f2d6cb176ddebcf8d65b453d78525708ead
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-10-13 11:14:21 +0200

    add mockDB with list of genres

---
+++ src/mockDB.js
@@ -0,0 +1,44 @@
+const genres = [
+  'Science fiction',
+  'Satire',
+  'Drama',
+  'Action and Adventure',
+  'Romance',
+  'Mystery',
+  'Horror',
+  'Self help',
+  'Health',
+  'Guide',
+  'Travel',
+  "Children's",
+  'Religion, Spirituality & New Age',
+  'Science',
+  'History',
+  'Math',
+  'Anthology',
+  'Poetry',
+  'Encyclopedias',
+  'Dictionaries',
+  'Comics',
+  'Art',
+  'Cookbooks',
+  'Diaries',
+  'Journals',
+  'Prayer books',
+  'Series',
+  'Trilogy',
+  'Biographies',
+  'Autobiographies',
+  'Fantasy'
+];
+
+function getGenres() {
+  return new Promise(res => {
+    setTimeout(() => {
+      res(genres);
+    }, 1000);
+  });
+}
+export default {
+  getGenres
+};
