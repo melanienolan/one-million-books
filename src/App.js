b71ba9b891adf1bc32304254032bf847cb77f52d
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-10-14 15:59:07 +0200

    add numberOfBooks variable, BookList component, onClick to generate books

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import faker from 'faker';
-import db from './mockDB';
+import { genres } from './mockDB';
+import BookList from './Components/BookList';
 import logo from './logo.svg';
 import './App.css';
+
+const numberOfBooks = 100;
 
 class App extends Component {
   constructor() {
@@ -13,29 +16,33 @@ class App extends Component {
       isLoading: true
     };
   }
+
   getGenres() {
-    db.getGenres().then(genres => {
-      this.setState({
-        genres,
-        isLoading: false
-      });
+    // db.getGenres().then(genres => {
+    //   this.setState({
+    //     genres,
+    //     isLoading: false
+    //   });
+    // });
+    this.setState({
+      genres,
+      isLoading: false
     });
   }
   componentDidMount() {
     this.getGenres();
-    this.generateBooks();
+    // this.generateBooks();
   }
   generateBooks() {
-    let id = 0;
     const capitalizeWords = str => {
       return str.replace(/\w\S*/g, txt => {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
       });
     };
     const genres = this.state.genres;
-    const numberOfBooks = 10000;
-    const books = [...Array(numberOfBooks)].map(x => {
-      id++;
+
+    const books = [...Array(numberOfBooks)].map((x, i) => {
+      const id = i;
       const title = capitalizeWords(faker.lorem.words());
       const authorFirstName = faker.name.firstName();
       const authorLastName = faker.name.lastName();
@@ -61,31 +68,35 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="">
-        <header className="">
-          <h1 className="">One Million Books...</h1>
-        </header>
-        <section className="filters">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-        </section>
-        <main className="books--container-empty">
-          <h3>Click here to generate books</h3>
-          <button className="button--generate" />
-        </main>
-        <main className="books--container-full">
-          <ul className="books--list">
-            <li className="books--item">One item</li>
-            <li className="books--item">One item</li>
-            <li className="books--item">One item</li>
-          </ul>
-        </main>
-      </div>
-    );
+    if (this.state.isLoading) {
+      return <div className="loading">Loading...</div>;
+    } else {
+      return (
+        <div className="">
+          <header className="">
+            <h1 className="">One Million Books...</h1>
+          </header>
+          <section className="filters">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+          </section>
+          {this.state.books.length < numberOfBooks
+            ? <main className="book--container-empty">
+                <h3>Click here to generate books</h3>
+                <button
+                  className="button--generate"
+                  onClick={() => this.generateBooks()}
+                />
+              </main>
+            : <main className="book--container-full">
+                <BookList books={this.state.books} />
+              </main>}
+        </div>
+      );
+    }
   }
 }
 
