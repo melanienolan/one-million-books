ff1e95b473f08f1229abdf9d4a9fbf4a50f56ef5
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-10-17 14:45:12 +0200

    add lastFridays function to generate list of last fridays

--- src/mockDB.js
+++ src/mockDB.js
@@ -1,5 +1,6 @@
 export const genres = [
   'All',
+  'Finance',
   'Science fiction',
   'Satire',
   'Drama',
@@ -33,6 +34,61 @@ export const genres = [
   'Fantasy'
 ];
 export const genders = ['All', 'Male', 'Female'];
+export const specialDates = [
+  'All',
+  'Halloween',
+  'Last Friday of Month',
+  'Halloween and Last Friday'
+];
+
+export const lastFridays = (() => {
+  const lastWeekDaysOfYear = (iWeekDay, y) =>
+    [
+      31,
+      (0 === y % 4 && 0 !== y % 100) || 0 === y % 400 ? 29 : 28,
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31
+    ].map(
+      (d, m) =>
+        new Date(
+          Date.UTC(y, m, d - (new Date(Date.UTC(y, m, d)).getDay() + (7 - iWeekDay)) % 7)
+        )
+    );
+  const days = {
+    sunday: 0,
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6
+  };
+  const curry = f => a => b => f(a, b);
+  const isoDateString = dte => dte.toISOString().substr(0, 10);
+  const range = (m, n) =>
+    Array.from(
+      {
+        length: Math.floor(n - m) + 1
+      },
+      (_, i) => m + i
+    );
+  const currentYear = new Date().getUTCFullYear();
+  const initialYear = currentYear - 100;
+  const flatten = arr => [].concat(...arr);
+  return flatten(
+    range(initialYear, currentYear)
+      .map(curry(lastWeekDaysOfYear)(days.friday))
+      .map(row => row.map(isoDateString))
+  );
+})();
 
 // function getGenres() {
 //   return new Promise(res => {
