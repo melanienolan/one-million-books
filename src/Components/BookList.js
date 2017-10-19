09720c79d74532bae547e76148cb03be0424d1b1
melanie.nolan@yahoo.co.uk <melly@Melanies-MacBook-Pro.local>
2017-10-19 12:10:53 +0200

    add number of books showing and fix error message

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -5,29 +5,39 @@ import 'react-virtualized/styles.css';
 
 const BookList = ({ books }) => {
   books = books.filter(book => book.visible);
-  const errorMessage = `No books exist with those filters`;
+  const errorMessage = 'No books exist with those filters';
   if (!books.length) {
     return (
       <div className="table--empty">
-        {errorMessage}
+        <div className="table--error">
+          {errorMessage}
+        </div>
+        <p className="table--number">
+          Number of books showing: {books.length}
+        </p>
       </div>
     );
   }
   return (
-    <Table
-      width={660}
-      height={360}
-      headerHeight={40}
-      rowHeight={30}
-      rowCount={books.length}
-      rowGetter={({ index }) => books[index]}>
-      <Column label="Id" dataKey="id" width={50} />
-      <Column label="Title" dataKey="title" width={210} />
-      <Column label="Author Name" dataKey="authorName" width={110} />
-      <Column label="Author Gender" dataKey="authorGender" width={100} />
-      <Column label="Genre" dataKey="genre" width={110} />
-      <Column label="Published" dataKey="published" width={80} />
-    </Table>
+    <div>
+      <Table
+        width={660}
+        height={360}
+        headerHeight={40}
+        rowHeight={30}
+        rowCount={books.length}
+        rowGetter={({ index }) => books[index]}>
+        <Column label="Id" dataKey="id" width={50} />
+        <Column label="Title" dataKey="title" width={210} />
+        <Column label="Author Name" dataKey="authorName" width={110} />
+        <Column label="Author Gender" dataKey="authorGender" width={100} />
+        <Column label="Genre" dataKey="genre" width={110} />
+        <Column label="Published" dataKey="published" width={80} />
+      </Table>
+      <p className="table--number">
+        Number of books showing: {books.length}
+      </p>
+    </div>
   );
 };
 
