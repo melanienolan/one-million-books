65789110fa931f0e60fa9696200a75b0479c6630
melanie.nolan@yahoo.co.uk <melly@Melanies-MacBook-Pro.local>
2017-10-19 12:10:21 +0200

    improve generateBooks function to use for loop and speed up, improve isDateSpecial function

--- src/mockDB.js
+++ src/mockDB.js
@@ -46,6 +46,7 @@ let specialDates = [
   'Halloween and Last Friday'
 ];
 
+/**** New generateBooks function, takes 3 secs ****/
 const generateBooks = numberOfBooks => {
   const capitalizeWords = str => {
     return str.replace(/\w\S*/g, txt => {
@@ -54,36 +55,44 @@ const generateBooks = numberOfBooks => {
   };
 
   const isDateSpecial = date => {
-    const isLastFriday =
-      isFriday(date) && differenceInDays(lastDayOfMonth(date), date) < 7 ? true : false;
-    if (isLastFriday && date.slice(5) === '10-31') {
-      return 'Halloween and Last Friday';
-    }
-    if (date.slice(5) === '10-31') {
-      return 'Halloween';
-    }
-    if (isLastFriday) {
-      return 'Last Friday of Month';
-    } else {
+    const isAFriday = isFriday(date);
+    const isHalloween = date.slice(5) === '10-31';
+    if (!isAFriday) {
+      if (isHalloween) return 'Halloween';
       return null;
     }
+    const isLastFriday = differenceInDays(lastDayOfMonth(date), date) < 7 ? true : false;
+    if (!isLastFriday) return null;
+    if (isHalloween) return 'Halloween and Last Friday';
+    return 'Last Friday of Month';
   };
+
   // remove All from list of genres and genders
   const _genres = genres.slice(1);
   const _genders = genders.slice(1);
-
-  const books = [...Array(numberOfBooks)].map((_, i) => {
+  let books = [],
+    i;
+  for (i = 0; i < numberOfBooks; i++) {
+    let authorName, authorGender, published, special;
+    if (i > 10000) {
+      let book = books[Math.floor(Math.random() * books.length)];
+      authorName = book.authorName;
+      authorGender = book.authorGender;
+      published = book.published;
+      special = book.special;
+    } else {
+      const authorFirstName = faker.name.firstName();
+      const authorLastName = faker.name.lastName();
+      authorName = `${authorLastName}, ${authorFirstName}`;
+      authorGender = _genders[Math.floor(Math.random() * _genders.length)];
+      published = faker.date.past(40).toISOString().substr(0, 10);
+      special = isDateSpecial(published);
+    }
     const id = i;
     const title = capitalizeWords(faker.lorem.words());
-    const authorFirstName = faker.name.firstName();
-    const authorLastName = faker.name.lastName();
-    const authorName = `${authorLastName}, ${authorFirstName}`;
-    const authorGender = _genders[Math.floor(Math.random() * _genders.length)];
     const genre = _genres[Math.floor(Math.random() * _genres.length)];
-    const published = faker.date.past(100).toISOString().substr(0, 10);
-    const special = isDateSpecial(published);
     const visible = 1;
-    return {
+    books.push({
       id,
       title,
       authorName,
@@ -92,10 +101,64 @@ const generateBooks = numberOfBooks => {
       published,
       special,
       visible
-    };
-  });
+    });
+  }
+
   return books;
 };
+
+/**** Original generateBooks function, takes 11 secs ****/
+
+// const generateBooks = numberOfBooks => {
+//   console.time('generate');
+//   const capitalizeWords = str => {
+//     return str.replace(/\w\S*/g, txt => {
+//       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//     });
+//   };
+//
+// const isDateSpecial = date => {
+//   const isAFriday = isFriday(date);
+//   const isHalloween = date.slice(5) === '10-31';
+//   if (!isAFriday) {
+//     if (isHalloween) return 'Halloween';
+//     return null;
+//   }
+//   const isLastFriday = differenceInDays(lastDayOfMonth(date), date) < 7 ? true : false;
+//   if (!isLastFriday) return null;
+//   if (isHalloween) return 'Halloween and Last Friday';
+//   return 'Last Friday of Month';
+// };
+//   // remove All from list of genres and genders
+//   const _genres = genres.slice(1);
+//   const _genders = genders.slice(1);
+//
+//   const books = [...Array(numberOfBooks)].map((_, i) => {
+//     const id = i;
+//     const title = capitalizeWords(faker.lorem.words());
+//     const authorFirstName = faker.name.firstName();
+//     const authorLastName = faker.name.lastName();
+//     const authorName = `${authorLastName}, ${authorFirstName}`;
+//     const authorGender = _genders[Math.floor(Math.random() * _genders.length)];
+//     const genre = _genres[Math.floor(Math.random() * _genres.length)];
+//     const published = faker.date.past(100).toISOString().substr(0, 10);
+//     const special = isDateSpecial(published);
+//     const visible = 1;
+//     return {
+//       id,
+//       title,
+//       authorName,
+//       authorGender,
+//       genre,
+//       published,
+//       special,
+//       visible
+//     };
+//   });
+//   console.timeEnd('generate');
+//   return books;
+// };
+
 const books = generateBooks(1000000);
 
 export default { books, genres, genders, specialDates };
